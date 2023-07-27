ed2f8d81699598380e109551d88c3dd7d54b3a15
Son Cao <cvson@utexas.edu>
2023-07-27 09:59:12 +0700

    Add files via upload

---
+++ globes/createFakeData_forFit.C
@@ -0,0 +1,15 @@
+void createFakeData_forFit(){
+    Bool_t isApplyingPoissonFluc = false;
+    TFile *pfile = new TFile("eventrate_all_t2k2_final_wsmear.root","READ");
+    const int NRULES = 4;
+    TH1D *hrate_all_rule[NRULES];
+    for (Int_t irule=0; irule<NRULES; ++irule) {
+        hrate_all_rule[irule] = (TH1D*)pfile->Get(Form("hrate_all_rule%d",irule));
+        cout<<"for rule "<<irule<<endl;
+        for (Int_t ibin=0; ibin<hrate_all_rule[irule]->GetNbinsX(); ++ibin) {
+            cout<<(int)hrate_all_rule[irule]->GetBinContent(ibin+1)<<", ";
+        }
+        cout<<endl;
+    }
+    
+}
