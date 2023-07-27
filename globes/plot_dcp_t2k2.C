202f4ef9264344787c09ed6e4e34a4c7dd087397
Son Cao <cvson@utexas.edu>
2023-07-27 07:03:30 +0700

    for dcp sensitivity

---
+++ globes/plot_dcp_t2k2.C
@@ -0,0 +1,110 @@
+void titleStyle(TH1* h1);
+void plot_dcp_t2k2(){
+    char *pfilename[] = {
+        "t2k2_final_nosmear_sensi_cp",
+        "t2k2_final_wsmear_sensi_cp"
+    };
+
+    char *plegname[] = {
+        "T2K-2 wo/ smearing",
+        "T2K-2 w/ smearing "
+    };
+    
+    Int_t NCOMP =sizeof(pfilename)/sizeof(pfilename[0]);
+    
+    char *phistname[] = {
+        "hchisq_min_glob_proj",/*for projection without knowing the mass hierarchy*/
+        "hchisq_min_nh_proj",/*projection with the mass hierachy known*/
+        "hchisq_min_glob_sys",
+        "hchisq_min_nh_nh_sys"
+    };
+    
+    Int_t NHIST =sizeof(phistname)/sizeof(phistname[0]);
+    TFile **pfile = new TFile*[NCOMP];
+    TH1D *hhist[4][4];
+    TLegend* leg0 = new TLegend(.42, .62, 0.9, .92);
+    leg0->SetFillStyle(0);
+    leg0->SetBorderSize(0);
+    leg0->SetTextSize(18);
+    leg0->SetTextFont(43);
+    
+    
+    const char *colorcode[] = { "#000000", "#0072B2","#D55E00","#CC79A7","#E69F00","#009E73","#56B4E9","#F0E442","#696969"};
+    Int_t ci;
+    
+    for (Int_t icomp=0; icomp<NCOMP; ++icomp) {
+        pfile[icomp] = new TFile(Form("%s.root",pfilename[icomp]),"READ");
+        for (Int_t ihist=0; ihist<NHIST; ++ihist) {
+            hhist[icomp][ihist] = (TH1D*)pfile[icomp]->Get(Form("%s",phistname[ihist]));
+            ci = TColor::GetColor(colorcode[icomp+1]);
+            hhist[icomp][ihist]->SetLineColor(ci);
+            hhist[icomp][ihist]->SetLineWidth(2);
+        }
+        leg0->AddEntry(hhist[icomp][0], Form("%s",plegname[icomp]), "l");
+        
+    }
+    
+    //T2K2 official
+    TFile *pfilet2k2 = new TFile("t2k2Scale_frt2k2017.root","READ");
+    char* histnamecp[] = {"t2k2_dcpexcl_knownmh_th23e0d5","t2k2_dcpexcl_knownmh_th23e0d43","t2k2_dcpexcl_knownmh_th23e0d6","t2k2_dcpexcl_unknownmh_th23e0d5","t2k2_dcpexcl_unknownmh_th23e0d43","t2k2_dcpexcl_unknownmh_th23e0d6"};
+    
+    Int_t NHISTDCP =sizeof(histnamecp)/sizeof(histnamecp[0]);
+    TGraph **dcpgraph = new TGraph*[NHISTDCP];
+    for (Int_t ihist=0; ihist<NHISTDCP; ++ihist) {
+        dcpgraph[ihist] = (TGraph*)pfilet2k2->Get(Form("%s",histnamecp[ihist]));
+    }
+    
+    //convert TGraph to TH1 using Eval
+    TH1D **dcphist = new TH1D*[NHISTDCP];
+    for (Int_t ihist=0; ihist<NHISTDCP; ++ihist) {
+        dcphist[ihist] = (TH1D*)hhist[0][0]->Clone(Form("dcphist_%d",ihist));
+        for (Int_t ibin=1; ibin<=hhist[0][0]->GetNbinsX(); ++ibin) {
+            double xcenter = (hhist[0][0]->GetBinCenter(ibin))*180./TMath::Pi();// convert from rad to degree
+            
+            double xchisqval = dcpgraph[ihist]->Eval(xcenter,0,"");//to convert to sigma
+            if (xchisqval<0) {
+                xchisqval=0;
+            }
+            if(ihist==0)cout<<"bin "<<ibin<<" center "<<xcenter<<" val "<<TMath::Sqrt(xchisqval)<<endl;
+            dcphist[ihist]->SetBinContent(ibin,TMath::Sqrt(xchisqval));
+        }
+        ci = TColor::GetColor(colorcode[0]);
+        dcphist[ihist]->SetLineColor(ci);
+        
+    }
+    leg0->AddEntry(dcphist[0], "T2K-2 paper", "l");
+    
+    for (Int_t ihist=0; ihist<NHIST; ++ihist) {
+        new TCanvas;
+        gStyle->SetOptStat(0);
+        
+        hhist[0][ihist]->Draw("hist");
+        hhist[0][ihist]->GetYaxis()->SetRangeUser(0,6);
+        hhist[0][ihist]->GetYaxis()->SetTitle("Sigma to exclude sin#delta_{CP}=0");
+        hhist[0][ihist]->GetXaxis()->SetTitle("True values of #delta_{CP} [rad]");
+        titleStyle(hhist[0][ihist]);
+        for (Int_t icomp=1; icomp<NCOMP; ++icomp) {
+            hhist[icomp][ihist]->Draw("hist same");
+        }
+        if(ihist==0 || ihist==2) dcphist[3]->Draw("hist same");// if mass hierarchy is not known
+        else dcphist[0]->Draw("hist same");// if mass hiearchy is known
+        leg0->Draw();
+        gPad->Print(Form("%s_comp.pdf",phistname[ihist]));
+        
+    }
+}
+
+void titleStyle(TH1* h1){
+    h1->SetTitle("");
+    h1->GetYaxis()->CenterTitle();
+    h1->GetXaxis()->CenterTitle();
+    h1->GetXaxis()->SetLabelSize(h1->GetXaxis()->GetTitleSize()*1.2);
+    h1->GetYaxis()->SetLabelSize(h1->GetYaxis()->GetTitleSize()*1.2);
+    h1->GetXaxis()->SetTitleSize(h1->GetXaxis()->GetLabelSize()*1.2);
+    h1->GetYaxis()->SetTitleSize(h1->GetYaxis()->GetLabelSize()*1.2);
+    
+    h1->GetYaxis()->SetTitleOffset(0.9);
+    h1->GetXaxis()->SetTitleOffset(0.94);
+}
+
+
